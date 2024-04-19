0086e28bf85b50dafd9ad23c08b9aedb5f089b1d
cnatanae <cnatanae@student.42sp.org.br>
2024-04-19 18:40:14 -0300

    Create a main tester for garbage collector

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 08:19:03 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/04/08 09:03:45 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/04/19 18:30:54 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,30 @@
 
 int	main(void)
 {
-	ft_printf("Mini \033[91mHELL\033[0m is Coming\n");
+	char	*str;
+	char	*str2;
+	int		idx;
+	str = (char *)allocate(sizeof(char) * 10);
+	str2 = (char *)allocate(sizeof(char) * 10);
+	if (str)
+	{
+		idx = 0;
+		while(idx < 10)
+		{
+			str[idx] = 'a';
+			idx++;
+		}
+	}
+	if (str2)
+	{
+		idx = 0;
+		while(idx < 10)
+		{
+			str2[idx] = 'b';
+			idx++;
+		}
+	}
+	ft_printf("%s\n", str);
+	ft_printf("%s\n", str2);
 	return (0);
 }
