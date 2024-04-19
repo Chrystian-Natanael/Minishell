c9971cf8a6a5adf4fac0077fbd429039537db728
cnatanae <cnatanae@student.42sp.org.br>
2024-04-19 18:39:52 -0300

    Added garbage_collector.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 08:25:10 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/04/09 10:03:19 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/04/19 17:49:22 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 /*For use libft functions*/
 # include "libft.h"
+# include "garbage_collector.h"
 
 /*For use system functions */
 # include <stdio.h>
