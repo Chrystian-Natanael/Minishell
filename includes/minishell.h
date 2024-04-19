d0cc13ebce52793b44395d4810839f6f446f8ba4
cnatanae <cnatanae@student.42sp.org.br>
2024-04-19 14:46:40 -0300

    Init the develop branch

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,17 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 08:25:10 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/04/08 08:26:35 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/04/09 10:03:19 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+/*For use libft functions*/
 # include "libft.h"
+
+/*For use system functions */
+# include <stdio.h>
 
 #endif
