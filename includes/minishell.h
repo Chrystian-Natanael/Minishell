10ab4168154e827e4cfcae2b287dfb52b0f30279
cnatanae <cnatanae@student.42sp.org.br>
2024-04-22 17:08:55 -0300

    Added include for use readline()

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 08:25:10 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/04/19 17:49:22 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/04/22 16:48:33 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 /*For use libft functions*/
 # include "libft.h"
 # include "garbage_collector.h"
-
+# include <readline/history.h>
+# include <readline/readline.h>
 /*For use system functions */
 # include <stdio.h>
 
