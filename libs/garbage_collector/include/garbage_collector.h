f6e4bc00cc4f59e96e103c694d9388b44c6e1187
cnatanae <cnatanae@student.42sp.org.br>
2024-04-22 12:51:45 -0300

    Added documantation of the functions

--- libs/garbage_collector/include/garbage_collector.h
+++ libs/garbage_collector/include/garbage_collector.h
@@ -6,17 +6,35 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:22:18 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/04/22 11:06:29 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/04/22 12:50:30 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GARBAGE_COLLECTOR_H
-#define GARBAGE_COLLECTOR_H
+# define GARBAGE_COLLECTOR_H
 
-#include "libft.h"
+/**
+ * @file garbage_collector.h
+ * @brief This file contains the garbage collector functions
+ * @author @Chrystian-Natanael && @kellyhayd
+ * @date 2024/04/19
+*/
 
+/*For use libft functions*/
+# include "libft.h"
+
+/*define t_typetree as pointer of struct s_typetree*/
 typedef struct s_typetree	*t_typetree;
 
+/**
+ * @brief struct s_typetree
+ * @details struct s_typetree is a binary tree that stores the addresses
+ *  of the allocated memory
+ * @param ltree is the left node of the binary tree
+ * @param rtree is the right node of the binary tree
+ * @param address is the address of the allocated memory
+ * @param height is the height of the binary tree
+*/
 struct s_typetree {
 	t_typetree		ltree;
 	t_typetree		rtree;
@@ -24,17 +42,86 @@ struct s_typetree {
 	int				height;
 };
 
-
+/**
+ * @brief allocate
+ * @details allocate memory
+ * @param size is the size of the memory to be allocated
+ * @return void* is the address of the allocated memory
+*/
 void		*allocate(unsigned int size);
+
+/**
+ * @brief deallocate
+ * @details deallocate specific memory address and your children
+ * @param address is the address of the memory to be deallocated
+*/
 void		deallocate(void *address);
+
+/**
+ * @brief quit
+ * @details destoy the binary tree and exit the program with the exit code
+ * @param code is the exit code
+*/
 void		quit(int code);
+
+/**
+ * @brief typetree_create
+ * @details create a new binary tree node
+ * @param address is the address of the allocated memory to be stored in
+ * the binary tree
+ * @return t_typetree is the new binary tree node
+*/
 t_typetree	typetree_create(void *address);
+
+/**
+ * @brief typetree_get_height
+ * @details get the height of the binary tree node
+ * @param typetree is the binary tree node
+ * @return int is the height of the binary tree node
+*/
 int			typetree_get_height(t_typetree typetree);
+
+/**
+ * @brief typetree_update_height
+ * @details update the height of the binary tree node
+ * @param typetree is the binary tree node to be updated
+*/
 void		typetree_update_height(t_typetree typetree);
+
+/**
+ * @brief typetree_insert
+ * @details insert a address in the binary tree
+ * @param address is the address of the allocated memory to be stored in
+ * the binary tree
+*/
 void		typetree_insert(void *address);
+
+/**
+ * @brief typetree_rebalance
+ * @details rebalance the binary tree
+ * @param typetree is the binary tree
+*/
 void		typetree_rebalance(t_typetree *typetree);
+
+/**
+ * @brief typetree_get
+ * @details get the root binary tree
+ * @return t_typetree is the root binary tree
+*/
 t_typetree	*typetree_get(void);
+
+/**
+ * @brief typetree_delete
+ * @details delete a address in the binary tree
+ * @param address is the address of the allocated memory to be deleted in
+ * the binary tree
+*/
 void		typetree_delete(void *address);
+
+/**
+ * @brief typetree_destroy
+ * @details destroy all binary tree
+*/
 void		typetree_destroy(void);
 
 #endif
