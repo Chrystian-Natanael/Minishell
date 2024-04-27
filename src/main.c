6557907f681750fb849fa2b069b746ade47eff34
Chrystian Natanael <chrystian.natanael.msn@gmail.com>
2024-04-27 17:48:29 -0300

    Added color in messages

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 08:19:03 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/04/27 17:21:00 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/04/27 17:45:14 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ int	main(void)
 {
 	char	*line;
 	
-	ft_printf("Welcome to Minishell, enjoy!\n");
-	line = readline("minishell$: ");
+	ft_printf("\033[92mWelcome to Minishell, enjoy! \033[0m\n");
+	line = readline("\033[94mminishell$: \033[0m");
 	while (line)
 	{
 		if (line[0] != '\0')
 			add_history(line);
 		typetree_insert(line);
-		line = readline("minishell$: ");
+		line = readline("\033[94mminishell$: \033[0m");
 	}
 	rl_clear_history();
-	ft_printf("Ending Minishell, Thanks!\n");
+	ft_printf("\033[91mEnding Minishell\033[0m, \033[92mThanks!\033[0m\n");
 	quit(0);
 }
