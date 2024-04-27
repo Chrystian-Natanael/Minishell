7dc73e3ebb58a8958b9551093be8f29d9b6340b0
Chrystian Natanael <chrystian.natanael.msn@gmail.com>
2024-04-27 19:54:17 -0300

    Remove static function

--- src/main.c
+++ src/main.c
@@ -6,13 +6,11 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 08:19:03 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/04/27 19:30:00 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/04/27 19:50:24 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static char	*get_username(t_envp *envp);
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -35,19 +33,4 @@ int	main(int argc, char **argv, char **envp)
 	rl_clear_history();
 	ft_printf("\033[91mEnding Minishell\033[0m, \033[92mThanks!\033[0m\n");
 	quit(0);
-}
-
-static char	*get_username(t_envp *envp) // ! por num utils depois
-{
-	char	*username;
-	char	*str1;
-	char	*str2;
-	username = envp_get("USER", envp);
-	if (!username)
-		username = "minishell";
-	str1 = ft_strjoin(username, "$: \033[0m");
-	str2 = ft_strjoin("\033[92m", str1);
-	typetree_insert(str1);
-	typetree_insert(str2);
-	return (str2);
 }
