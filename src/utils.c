fc7d3f7e7d3af252541a548b1a96d46c51f245f7
cnatanae <cnatanae@student.42sp.org.br>
2024-05-01 12:46:24 -0300

    Added path in getuser

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 19:50:30 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/05/01 09:28:44 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/05/01 11:17:56 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,27 @@
 char	*get_username(t_envp *envp)
 {
 	char	*username;
+	char	*final_string;
+	char	*pwd;
 	char	*str1;
 	char	*str2;
 
 	username = envp_get("USER", envp);
+	pwd = envp_get("PWD", envp);
 	if (!username)
 		username = "minishell";
-	str1 = ft_strjoin(username, "$: \033[0m");
-	str2 = ft_strjoin("\033[92m", str1);
-	typetree_insert(str1);
+	str2 = ft_strrchr(pwd, '/');
+	if (!str2)
+		str2 = "//";
+	str1 = ft_strjoin(++str2, "$\033[0m ");
+	str2 = ft_strjoin("\033[92m", username);
 	typetree_insert(str2);
-	return (str2);
+	str2 = ft_strjoin(str2, "\033[0m:\033[34m");
+	final_string = ft_strjoin(str2, str1);
+	typetree_insert(final_string);
+	typetree_insert(str2);
+	typetree_insert(str1);
+	return (final_string);
 }
 
 void	free_split(char **array)
