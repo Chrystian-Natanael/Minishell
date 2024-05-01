ac8746cf60fa3e7fb91e8089444d79678b433cd3
cnatanae <cnatanae@student.42sp.org.br>
2024-05-01 09:01:51 -0300

    Added tokenizer in main loop

--- src/main.c
+++ src/main.c
@@ -6,16 +6,18 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 08:19:03 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/04/27 17:45:14 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/05/01 08:39:06 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
 int	main(void)
 {
 	char	*line;
-	
+	t_token	*token;
+
 	ft_printf("\033[92mWelcome to Minishell, enjoy! \033[0m\n");
 	line = readline("\033[94mminishell$: \033[0m");
 	while (line)
@@ -24,6 +26,8 @@ int	main(void)
 			add_history(line);
 		typetree_insert(line);
 		line = readline("\033[94mminishell$: \033[0m");
+		token = lexer(line);
+		print_tokens(token);
 	}
 	rl_clear_history();
 	ft_printf("\033[91mEnding Minishell\033[0m, \033[92mThanks!\033[0m\n");
