0661dfba19fb5a0d1621028770439acb7328768e
Kelly Rocha Hayd <krocha-h@c1r5p4.42sp.org.br>
2024-05-01 19:02:06 -0300

    tests working in specific cases; to compare the words of expected and result, it needs to use another function (ft_isequal) to compare each character of the string; needs to ajust names

--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-static t_token *create(int num_args, ...)
+static t_token *create_with_enum(int num_args, ...)
 {
 	t_token *result;
 	va_list	ap;
-	
+
 	result = NULL;
 	va_start(ap, num_args);
 	while (num_args != 0)
@@ -18,28 +18,54 @@ static t_token *create(int num_args, ...)
 	return (result);
 }
 
+int	ft_isequal(const char *a, const char *b)
+{
+	int idx = 0;
+
+	while (a && b && a[idx] && b[idx])
+	{
+		if (a[idx] != b[idx])
+			return (0);
+		idx++;
+	}
+	return (1);
+}
+
 static int	is_equal(const t_token *a, const t_token *b)
 {
 	if (a == b)
 		return (1);
 	while (a != NULL && b != NULL)
 	{
-		printf("%d %d \n", a->type, b->type);
 		if (a->type != b->type || a->lexema != b->lexema)
 			return (0);
 		a = a->next;
 		b = b->next;
 	}
-	return (a == b);
+	return (1);
 }
 
-static int	test_symbols()
+static int	test_word_simple_quote()
+{
+	char	*word = "\'kjahsihsa dihhdakdha\'";
+	t_token	*expected = NULL;
+	t_token	*result;
+
+	expected = malloc(sizeof(t_token));
+	expected->lexema = "kjahsihsa dihhdakdha";
+	expected->type = WORD;
+	expected->next = NULL;
+	result = lexer(word);
+	return (ft_isequal(expected->lexema, result->lexema));
+}
+
+static int	test_symbols(void)
 {
 	char	*symbols = "    < && | >> << || $$ ( )";
 	t_token	*expected;
 	t_token	*result;
 
-	expected = create(10, REDIR_INPUT, AND, PIPE, OUTPUT_APPEND, HEREDOC, OR, DOLLAR, DOLLAR, L_PAREN, R_PAREN);
+	expected = create_with_enum(10, REDIR_INPUT, AND, PIPE, OUTPUT_APPEND, HEREDOC, OR, DOLLAR, DOLLAR, L_PAREN, R_PAREN);
 	result = lexer(symbols);
 	return (is_equal(expected, result));
 }
@@ -63,6 +89,11 @@ int	main () {
 	if (!test_symbols())
 	{
 		printf("failed test_symbols\n");
+		quit (EXIT_FAILURE);
+	}
+	if (!test_word_simple_quote())
+	{
+		printf("failed test_word_simple_quote\n");
 		quit (EXIT_FAILURE);
 	}
 	quit (EXIT_SUCCESS);
