d9779de092610832b6cd74d2d87d8909c4ceb36c
cnatanae <cnatanae@student.42sp.org.br>
2024-05-01 09:00:17 -0300

    Alterate return function for quit function (garb collector)

--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -35,11 +35,11 @@ static int	is_equal(const t_token *a, const t_token *b)
 
 static int	test_symbols()
 {
-	char	*symbols = "    < && | > << || $ ( )";
+	char	*symbols = "    < && | >> << || $$ ( )";
 	t_token	*expected;
 	t_token	*result;
 
-	expected = create(9, REDIR_INPUT, AND, PIPE, REDIR_OUTPUT, HEREDOC, OR, DOLLAR, L_PAREN, R_PAREN);
+	expected = create(10, REDIR_INPUT, AND, PIPE, OUTPUT_APPEND, HEREDOC, OR, DOLLAR, DOLLAR, L_PAREN, R_PAREN);
 	result = lexer(symbols);
 	return (is_equal(expected, result));
 }
@@ -58,12 +58,12 @@ int	main () {
 	if (!test_all_spaces_empty_list())
 	{
 		printf("failed test_all_spaces_empty_list\n");
-		return (EXIT_FAILURE);
+		quit (EXIT_FAILURE);
 	}
 	if (!test_symbols())
 	{
 		printf("failed test_symbols\n");
-		return (EXIT_FAILURE);
+		quit (EXIT_FAILURE);
 	}
-	return (EXIT_SUCCESS);
+	quit (EXIT_SUCCESS);
 }
