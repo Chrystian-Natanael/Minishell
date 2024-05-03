ef755193d64cb3cfae7d3907f24acb4a79a0722e
cnatanae <cnatanae@student.42sp.org.br>
2024-05-03 17:44:13 -0300

    Added two new tests for cmds parsing

--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -21,15 +21,22 @@ static t_token *create(int num_args, ...)
 static int	is_equal(const t_token *a, const t_token *b)
 {
 	if (a == b)
+	{
+		printf("\033[92m. \033[0m");
 		return (1);
+	}
 	while (a != NULL && b != NULL)
 	{
-		printf("%d %d \n", a->type, b->type);
+		// printf("%d %d \n", a->type, b->type);
 		if (a->type != b->type || a->lexema != b->lexema)
+		{
+			printf("\033[91m. \033[0m");
 			return (0);
+		}
 		a = a->next;
 		b = b->next;
 	}
+	printf("\033[92m. \033[0m");
 	return (a == b);
 }
 
@@ -59,23 +66,28 @@ int	expr_is_equal(t_token *expected, t_token *result)
 	while (expected != NULL && result != NULL)
 	{
 		if (expected->type != result->type || ft_strncmp(expected->lexema, result->lexema, ft_strlen(expected->lexema)))
+		{
+			printf("\033[91m. \033[0m");
 			return (0);
+		}
 		expected = expected->next;
 		result = result->next;
 	}
+	printf("\033[92m. \033[0m");
 	return (expected == result);
 }
 
-static int	test_two_commands()
+static int	test_three_expr()
 {
 	//! line = "ls -l || echo hello";
 	t_token	*expected;
 	t_token	*result;
 	t_token	*tokens;
 	
-	expected = create(2, EXPRESSION, EXPRESSION);
+	expected = create(3, EXPRESSION, OR, EXPRESSION);
 	expected->lexema = "ls -l";
-	expected->next->lexema = "echo hello";
+	expected->next->lexema = "||";
+	expected->next->next->lexema = "echo hello";
 	tokens = create(5, WORD, WORD, OR, WORD, WORD);
 	tokens->lexema = "ls";
 	tokens->next->lexema = "-l";
@@ -86,21 +98,56 @@ static int	test_two_commands()
 	return (expr_is_equal(expected, result));
 }
 
+static int	test_five_expr()
+{
+	//! line = "ls -l | grep a >> outfile.txt && echo file created";
+	t_token	*expected;
+	t_token	*result;
+	t_token	*tokens;
+
+	expected = create(5, EXPRESSION, PIPE, EXPRESSION, AND, EXPRESSION);
+	expected->lexema = "ls -l";
+	expected->next->lexema = "|";
+	expected->next->next->lexema = "grep a >> outfile.txt";
+	expected->next->next->next->lexema = "&&";
+	expected->next->next->next->next->lexema = "echo file created";
+	tokens = create(11, WORD, WORD, PIPE, WORD, WORD, OUTPUT_APPEND, WORD, AND, WORD, WORD, WORD);
+	tokens->lexema = "ls";
+	tokens->next->lexema = "-l";
+	tokens->next->next->lexema = "|";
+	tokens->next->next->next->lexema = "grep";
+	tokens->next->next->next->next->lexema = "a";
+	tokens->next->next->next->next->next->lexema = ">>";
+	tokens->next->next->next->next->next->next->lexema = "outfile.txt";
+	tokens->next->next->next->next->next->next->next->lexema = "&&";
+	tokens->next->next->next->next->next->next->next->next->lexema = "echo";
+	tokens->next->next->next->next->next->next->next->next->next->lexema = "file";
+	tokens->next->next->next->next->next->next->next->next->next->next->lexema = "created";	
+	result = cmd_parsing(tokens);
+	return (expr_is_equal(expected, result));
+}
+
 int	main () {
 	if (!test_all_spaces_empty_list())
 	{
-		printf("failed test_all_spaces_empty_list\n");
+		printf("\033[91mfailed test_all_spaces_empty_list\033[0m\n");
 		quit (EXIT_FAILURE);
 	}
 	if (!test_symbols())
 	{
-		printf("failed test_symbols\n");
+		printf("\033[91mfailed test_symbols\033[0m\n");
 		quit (EXIT_FAILURE);
 	}
-	if(!test_two_commands())
+	if(!test_three_expr())
 	{
-		printf("failed test_two_commands\n");
+		printf("\033[91mfailed test_three_expr\033[0m\n");
 		quit (EXIT_FAILURE);
 	}
+	if(!test_five_expr())
+	{
+		printf("\033[91mfailed test_five_expr\033[0m\n");
+		quit (EXIT_FAILURE);
+	}
+	printf("\n\n\033[92mAll tests passed\033[0m\n");
 	quit (EXIT_SUCCESS);
 }
