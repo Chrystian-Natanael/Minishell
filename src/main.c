f419a980861273ebe2987b490bc3a0209c27aacb
cnatanae <cnatanae@student.42sp.org.br>
2024-05-10 11:50:43 -0300

    Added verification if lexer return NULL

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 08:19:03 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/05/06 14:22:11 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/05/10 11:40:36 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,15 @@ int	main(int argc, char **argv, char **envp)
 			add_history(line);
 		typetree_insert(line);
 		token = lexer(line);
+		if (token == NULL)
+		{
+			ft_printf("\033[91mError: \033[0mInvalid syntax\n");
+			continue ;
+		}
 		expr = cmd_parsing(token);
 		// print_tokens(token);
 		print_tokens(expr);
+		// execute(expr, my_envp);
 	}
 	rl_clear_history();
 	ft_printf("\033[91mEnding Minishell\033[0m, \033[92mThanks!\033[0m\n");
