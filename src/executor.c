df7e355959282f8529cb0be5954d5a99953bb547
cnatanae <cnatanae@student.42sp.org.br>
2024-05-10 11:50:01 -0300

    Create file that execute cmds

---
+++ src/executor.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 15:44:27 by cnatanae          #+#    #+#             */
+/*   Updated: 2024/05/09 08:02:47 by cnatanae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file executor.c
+ * @brief This file contains the executor functions
+ * @author @Chrystian-Natanael && @kellyhayd
+ * @date 2024/05/06
+*/
+
+#include "minishell.h"
+
+
+void	execute(t_token *tokens, t_envp *envp)
+{
+	
+}
