f6a7cf97edb92ea86466548774f694d644b4d306
cnatanae <cnatanae@student.42sp.org.br>
2024-05-16 15:50:38 -0300

    Remove unused functions

--- src/syntax.c
+++ src/syntax.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 13:41:15 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/05/11 15:04:13 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/05/16 13:14:50 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,108 +67,4 @@ int	syntax_error(t_token *token)
 		tmp = tmp->next;
 	}
 	return (0);
-}
-
-void	organize_expressions(t_token **expr)
-{
-	t_token *tmp;
-
-	tmp = *expr;
-	while (tmp)
-	{
-		if (exist_redir(tmp->lexema) && !exist_parent(tmp->lexema))
-			tmp->lexema = organize_redir(tmp->lexema);
-		tmp = tmp->next;
-	}
-}
-
-int	exist_parent(char *str)
-{
-	int	i;
-
-	i = strlen(str);
-	if (str[0] == '(' && str[i - 1] == ')')
-		return (1);
-	return (0);
-}
-
-int	exist_redir(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '>' || str[i] == '<')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-char	*organize_redir(char *str)
-{
-	char	*new_str;
-	char	*tmp;
-	char	*aux;
-	int		i;
-
-	i = 0;
-	new_str = allocate(sizeof(char) * (ft_strlen(str) + 1));
-	tmp = NULL;
-	while (str[i])
-	{
-		if (str[i] == '>' || str[i] == '<')
-		{
-			if (tmp == NULL)
-			{
-				tmp = ft_substr(str, i++, 1);
-				typetree_insert(tmp);
-				tmp = ft_strjoin(" ", tmp);
-				typetree_insert(tmp);
-			}
-			else
-			{
-				tmp = ft_strjoin(tmp, " ");
-				typetree_insert(tmp);
-				aux = ft_substr(str, i++, 1);
-				typetree_insert(aux);
-				tmp = ft_strjoin(tmp, aux);
-				typetree_insert(tmp);
-			}
-			if (str[i] == '>' || str[i] == '<')
-			{
-				aux = ft_substr(str, i++, 1);
-				typetree_insert(aux);
-				tmp = ft_strjoin(tmp, aux);
-				typetree_insert(tmp);
-			}
-			tmp = ft_strjoin(tmp, " ");
-			typetree_insert(tmp);
-			while (ft_isspace(str[i]))
-				i++;
-			while (str[i] != '\0' && str[i] != ' ')
-			{
-				aux = ft_substr(str, i, 1);
-				typetree_insert(aux);
-				tmp = ft_strjoin(tmp, aux);
-				typetree_insert(tmp);
-				i++;
-			}
-		}
-		else
-		{
-			aux = ft_substr(str, i, 1);
-			typetree_insert(aux);
-			new_str = ft_strjoin(new_str, aux);
-			typetree_insert(new_str);
-			i++;
-		}
-	}
-	if (tmp != NULL)
-	{
-		new_str = ft_strjoin(new_str, tmp);
-		typetree_insert(new_str);
-	}
-	return (new_str);
 }
