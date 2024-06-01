4e293caee6f73fc4d4705e3e0ad2972da5120893
Kelly Hayd <hayd.kelly@gmail.com>
2024-06-01 20:31:08 -0300

    unset and exit builtin

---
+++ src/builtin/exit.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/01 20:05:19 by krocha-h          #+#    #+#             */
+/*   Updated: 2024/06/01 20:27:32 by krocha-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file pwd.c
+ * @brief This file contains the pwd function
+ * @author @Chrystian-Natanael && @kellyhayd
+ * @date 2024/06/01
+*/
+
+#include "minishell.h"
+
+void	minishell_exit(char **argv)
+{
+	int	count;
+
+	count = args_count(argv);
+	if (!ft_isalnum(argv[1]))
+	{
+		ft_putstr_fd("-minishell: exit: ", 2);
+		ft_putstr_fd(argv[1], 2);
+		ft_putstr_fd(": numeric argument required", 2);
+	}
+	else if (count > 2)
+		ft_putstr_fd("-minishell: exit: too many arguments\n", 2);
+	else if (count == 2 && ft_isalnum(argv[1]))
+		quit(ft_atoi(argv[1]));
+	else
+		quit(0);
+}
---
+++ src/builtin/unset.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/01 18:12:55 by krocha-h          #+#    #+#             */
+/*   Updated: 2024/06/01 19:43:55 by krocha-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file pwd.c
+ * @brief This file contains the pwd function
+ * @author @Chrystian-Natanael && @kellyhayd
+ * @date 2024/05/31
+*/
+
+#include "minishell.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 || *s2)
+	{
+		if (*s1 != *s2)
+			return ((*(unsigned char *)s1) - (*(unsigned char *)s2));
+		s1++;
+		s2++;
+	}
+	return (0);
+}
+
+void	unset(char **argv, t_envp **envp)
+{
+	t_envp	*prev;
+	t_envp	*curr;
+	
+	if (!argv[1])
+		return ;
+	curr = *envp;
+	while (curr && ft_strcmp(argv[1], curr->key) != 0)
+	{
+		prev = curr;
+		curr = curr->next;
+	}
+	if (ft_sdtrcmp(argv[1], curr->key) == 0)
+	{
+		if (curr == *envp)
+			*envp = (*envp)->next;
+		else
+			prev->next = curr->next;
+		deallocate(curr);
+	}
+}
