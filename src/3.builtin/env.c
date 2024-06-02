ecbbda32187934ac461bad1949100445586a5943
Kelly Hayd <hayd.kelly@gmail.com>
2024-06-02 19:42:55 -0300

    fix segfault: while (envp && (*envp)

--- src/3.builtin/env.c
+++ src/3.builtin/env.c
@@ -6,20 +6,20 @@
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 17:36:33 by krocha-h          #+#    #+#             */
-/*   Updated: 2024/06/02 15:37:35 by krocha-h         ###   ########.fr       */
+/*   Updated: 2024/06/02 19:41:36 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /**
- * @file pwd.c
- * @brief This file contains the pwd function
+ * @file env.c
+ * @brief This file contains the env function
  * @author @Chrystian-Natanael && @kellyhayd
  * @date 2024/06/01
 */
 
 #include "minishell.h"
 
-int	ft_env(char **argv, t_envp *envp)
+int	ft_env(char **argv, t_envp **envp)
 {
 	if (argv[1])
 	{
@@ -28,12 +28,12 @@ int	ft_env(char **argv, t_envp *envp)
 		ft_putstr_fd("\': No such file or directory\n", 2);
 		return (127);
 	}
-	while (envp)
+	while (envp && (*envp))
 	{
-		ft_putstr_fd(envp->key, 1);
-		ft_putstr_fd(envp->value, 1);
+		ft_putstr_fd((*envp)->key, 1);
+		ft_putstr_fd((*envp)->value, 1);
 		ft_putchar_fd('\n', 1);
-		envp = envp->next;
+		(*envp) = (*envp)->next;
 	}
 	return (0);
 }
