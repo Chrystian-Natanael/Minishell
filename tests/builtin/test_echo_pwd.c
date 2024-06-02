e2272e7bf40ebf949367031cbcf88046d0b55255
Kelly Hayd <hayd.kelly@gmail.com>
2024-06-02 18:11:54 -0300

    tests names updated

--- tests/builtin/test_echo_pwd.c
+++ tests/builtin/test_echo_pwd.c
@@ -1,29 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_builtin.c                                     :+:      :+:    :+:   */
+/*   test_echo_pwd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 15:10:23 by krocha-h          #+#    #+#             */
-/*   Updated: 2024/06/01 18:08:38 by krocha-h         ###   ########.fr       */
+/*   Updated: 2024/06/02 16:40:33 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
-#include <stdio.h>
 
 int	main()
 {
 	pwd();
+	ft_putchar_fd('\n', 1);
 	
-	char	*args[3];
+	char	*echos[3];
+	echos[0] = "echo";
+	echos[1] = "Kelly";
+	echos[2] = "Hayd";
+	echos[3] = '\0';
 	
-	args[0] = "echo";
-	args[1] = "Kelly";
-	args[2] = "Hayd";
-	args[3] = '\0';
+	echo(echos);
+	ft_putchar_fd('\n', 1);
+
+	char	*exit[3];
+	exit[0] = "exit";
+	exit[1] = "12";
+	exit[2] = "kelly";
+	exit(exit);
 	
-	echo(args);
 	return (0);
 }
