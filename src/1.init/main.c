c8a825a56059074d1f2254cdb32eb5f590c5a0ee
cnatanae <cnatanae@student.42sp.org.br>
2024-06-03 08:35:19 -0300

    Orgnized main

--- src/1.init/main.c
+++ src/1.init/main.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 08:19:03 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/06/03 08:09:37 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/06/03 08:18:37 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,30 +36,17 @@ int	main(int argc, char **argv, char **envp)
 	while ((line))
 	{
 		username = get_username(my_envp);
-		if (line[0] == '\0')
+		typetree_insert(line);
+		token = lexer(line);
+		if (line[0] == '\0' || token == NULL || syntax_error(token))
 		{
+			ft_printf("\033[91mError: \033[0mInvalid syntax\n");
 			line = readline(username);
 			continue ;
 		}
 		add_history(line);
-		typetree_insert(line);
-		token = lexer(line);
-		if (token == NULL)
-		{
-			ft_printf("\033[91mError: \033[0mInvalid syntax\n");
-			continue ;
-		}
-		if (syntax_error(token))
-		{
-			ft_printf("\033[91mError: \033[0mInvalid syntax\n");
-			continue ;
-		}
 		expr = cmd_parsing(token);
-		// print_tokens(expr);
-		// ft_printf("\n");
-		
 		execute(expr, &my_envp);
-		
 		line = readline(username);
 	}
 	rl_clear_history();
