9217b7b09c04e73ed04af8fcd58eaa9a16257c6f
cnatanae <cnatanae@student.42sp.org.br>
2024-06-03 10:55:22 -0300

    Change function of erros

--- libs/libft/source/libft/ft_error.c
+++ libs/libft/source/libft/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 10:27:06 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/06/03 08:55:00 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/06/03 09:12:05 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	ft_error(char *name, char *str1, char *str2, int ret_code)
 {
 	ft_putstr_fd(name, ERROR);
-	ft_putstr_fd(": ", ERROR);
 	ft_putstr_fd(str1, ERROR);
 	ft_putendl_fd(str2, ERROR);
 	return (ret_code);
